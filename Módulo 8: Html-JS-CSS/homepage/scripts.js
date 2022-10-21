77ef06fda1800ae9b6ab71a3cb297926cd850d5f
Lauro <103678530+laurocn@users.noreply.github.com>
2022-10-21 11:27:12 -0300

    html

---
+++ Módulo 8: Html-JS-CSS/homepage/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('submit', function() {
+        let name = document.querySelector('#name').value;
+        alert('hello, ' + name);
+    });
+});
